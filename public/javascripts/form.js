60cb394d553820e6ee0bd587a950683ebaef42bd
Shailee <mehtashailee21@gmail.com>
2017-09-04 00:37:14 +0530

    validations

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -7,6 +7,53 @@
         });
 
 
+        function reset() {
+            $('#sub').prop('disabled', true);
+            $("input#ip1").val('');
+            $("#ip2").val('');
+        }
+
+        reset()
+        $('#sub').prop('disabled', true);
+
+        $('#ip1').keyup(function() {
+                if ($('#ip1').val() == '') {
+                    $('#ip1').css('border-color', 'red');
+                } else {
+                    $('#ip1').css('border-color', 'black');
+
+                }
+                if ($(this).val() != '' && $('#ip2').val() !== '') {
+                    $('#sub').prop('disabled', false);
+                }
+            })
+            .focusout(function() {
+                if ($('#ip1').val() == '') {
+                    $('#ip1').css('border-color', 'red');
+                } else {
+                    $('#ip1').css('border-color', 'black');
+
+                }
+            });
+        $('#ip2').keyup(function() {
+                if ($(this).val() != '' && $('#ip1').val() !== '') {
+                    $('#sub').prop('disabled', false);
+                }
+                if ($('#ip2').val() == '') {
+                    $('#ip2').css('border-color', 'red');
+                } else {
+                    $('#ip2').css('border-color', 'black');
+
+                }
+            })
+            .focusout(function() {
+                if ($('#ip2').val() == '') {
+                    $('#ip2').css('border-color', 'red');
+                } else {
+                    $('#ip2').css('border-color', 'black');
+
+                }
+            });
 
         $(document).on('submit', 'form#selF', function(e) {
             e.preventDefault();
@@ -16,13 +63,15 @@
                 },
                 function(data, status) {
                     console.log(data, status)
-                    $("input#ip1").val('');
+                    reset()
                     // $("#respText").innerText = '';
 
                     if (!data) {
                         $("#respText").text("ERR!");
+                        $("#respText").addClass("has-error");
 
                     } else {
+                        $("#respText").addClass("has-success");
                         $("#respText").text(data);
                     }
                     //alert("Data: " + data + "\nStatus: " + status);
